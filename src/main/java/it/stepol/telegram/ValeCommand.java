2cbef4e1d5149f03af584b8e736ecae4deaee99e
stefano poli <stefano.poli@ict-group.it>
2016-05-30 18:28:05 +0200

    Logs

--- src/main/java/it/stepol/telegram/ValeCommand.java
+++ src/main/java/it/stepol/telegram/ValeCommand.java
@@ -6,11 +6,14 @@ import io.github.nixtabyte.telegram.jtelebot.request.TelegramRequest;
 import io.github.nixtabyte.telegram.jtelebot.request.factory.TelegramRequestFactory;
 import io.github.nixtabyte.telegram.jtelebot.response.json.Message;
 import io.github.nixtabyte.telegram.jtelebot.server.impl.AbstractCommand;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Stefano Poli on 30/05/16.
  */
 public class ValeCommand extends AbstractCommand {
+
+    private static final Logger LOG = Logger.getLogger(ValeCommandFactory.class);
 
     public ValeCommand(Message message, RequestHandler requestHandler) {
         super(message, requestHandler);
